59b661cd536a72cc692b9b2591ccf7e445ab460c
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-19 16:26:18 -0500

    Added M&C for tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const URL = 'https://api.thecatapi.com/v1/breedssearch?q=';
+const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
 
 const fetchBreedDescription = function(breedName, callback) {
@@ -8,11 +8,11 @@ const fetchBreedDescription = function(breedName, callback) {
       return callback(error);
     }
     if (response.statusCode !== 200) {
-      return callback(response.statusCode);
+      return callback(null);
     }
     const [data] = JSON.parse(body); // destructuring
     const search = [data][0];
-    if (!data) {
+    if (!data || !search.description) {
       callback(`The breed you just searched cannot be found`);
     } else {
       callback(null ,search.description);
